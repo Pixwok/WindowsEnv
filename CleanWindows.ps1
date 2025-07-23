5498eacea0c407a00330e41bf01520baf46fa157
Pixwok <40206880+Pixwok@users.noreply.github.com>
2025-07-23 18:28:12 +0200

    Update CleanWindows

--- CleanWindows.ps1
+++ CleanWindows.ps1
@@ -3,3 +3,7 @@ Get-AppxPackage *WindowsMaps* | Remove-AppxPackage
 Get-AppxPackage *BingWeather* | Remove-AppxPackage
 Get-AppxPackage *skype* | Remove-AppxPackage
 Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
+Get-AppxPackage *BingNews* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage
